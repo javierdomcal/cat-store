b11109302b863c77169cf50016aa9fc655f0bb01
Javier Dominguez <68706306+javierdomcal@users.noreply.github.com>
2023-04-19 16:55:05 +0200

    Add files via upload

---
+++ click.js
@@ -0,0 +1,4 @@
+// Toggle navigation on button click
+document.getElementById('nav-toggle').addEventListener('click', function() {
+  document.getElementById('nav-list').classList.toggle('nav-list--active');
+});
